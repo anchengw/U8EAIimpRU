c15c4b75a6ebea97f21a91f14c1956642392498d
anchengw <anchengw@163.com>
2018-09-03 14:08:54 +0800

    U8 EAI 本地COM导入

--- U8EAIimpRU/Form1.Designer.cs
+++ U8EAIimpRU/Form1.Designer.cs
@@ -33,6 +33,7 @@
             this.button3 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -76,7 +77,7 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(281, 12);
+            this.button4.Location = new System.Drawing.Point(266, 12);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 4;
@@ -84,11 +85,22 @@
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(360, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "EAI本地调用";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(519, 305);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button3);
@@ -107,6 +119,7 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- U8EAIimpRU/Form1.cs
+++ U8EAIimpRU/Form1.cs
@@ -24,7 +24,7 @@ namespace U8EAIimpRU
             StringBuilder sb = new StringBuilder();
             sb.Append("<ufinterface sender=\"001\" receiver=\"u8\" roottag=\"department\" docid=\" \" proc=\"Query\" codeexchanged=\"n \">")
                 .Append("<department importfile=\" \" exportfile=\" \" code=\"011\" bincrementout=\"n \">")
-                .Append("<field display=\"部门编码\" name=\"cDepCode\" operation=\" =\" value=\"1 \" logic=\" \" /> ")
+                .Append("<field display=\"部门编码\" name=\"cDepCode\" operation=\" =\" value=\"1\" logic=\" \" /> ")//查询过滤条件 field节点中的name过滤字段名要与数据库字段名一致
                 .Append("</department>")
                 .Append("</ufinterface>");
             XmlDocument dom = new XmlDocument();
@@ -148,6 +148,64 @@ namespace U8EAIimpRU
                 System.Runtime.InteropServices.Marshal.FinalReleaseComObject(xmlHttp);       //COM释放
             }
         }
-        
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, string> d1 = moban.clearCloneDictVal(moban.entryMoban);//克隆
+            //Dictionary<string, string> d1 = new Dictionary<string, string>(moban.entryMoban);//克隆
+            moban.rootAttr["sender"] = "001";
+            moban.rootAttr["roottag"] = moban.mobanType;
+            moban.rootAttr["proc"] = "add";
+
+            moban.headerDict["code"] = "0000000001";             //单据号 相同单据号时会自动编号，但不能为空
+            moban.headerDict["date"] = DateTime.Now.ToString(); //单据日期 入库日期
+            moban.headerDict["receiveflag"] = "1"; //收发标志 入库单-收 1，出库单-发 0
+            moban.headerDict["vouchtype"] = "10"; //'单据类型 10 产成品入库 
+            moban.headerDict["businesstype"] = "成品入库"; //业务类型
+            moban.headerDict["source"] = "库存"; //单据来源 采购、销售、库存、存货
+            moban.headerDict["warehousecode"] = "2"; //仓库编码
+            moban.headerDict["receivecode"] = "12";//收发类别编码
+            moban.headerDict["departmentcode"] = "";//部门编码
+            moban.headerDict["personcode"] = "";//职员编码 业务员
+
+            moban.headerDict["handler"] = "范磊";//经手人
+            moban.headerDict["maker"] = "范磊";//制单人
+            moban.headerDict["memory"] = "";//备注
+            moban.headerDict["chandler"] = "";//审核人
+            moban.headerDict["auditdate"] = "";//审核日期
+
+            d1["inventorycode"] = "MTVMC";//存货编码
+            d1["invname"] = "PVC密度板橱柜吊柜门";//存货名称
+            d1["free1"] = "YD01YG0-CG1";//商品代码 －吉屋用
+            d1["free2"] = "SF20088-03PCT";//表面色 －吉屋用
+            d1["quantity"] = "1";//数量
+            d1["cmassunitname"] = "套";//主记量单位名称
+            d1["price"] = "999";//单价
+            d1["cost"] = "999";//金额
+            d1["makedate"] = "";//生产日期
+            d1["define30"] = "YD01YG0001";//订单号 －吉屋用
+            d1["iexpiratdatecalcu"] = "0";//有效期推算方式
+
+
+            moban.entryList.Add(d1);
+            XmlDocument dom = moban.combineU8Xml();
+            //先要引用D:\U8SOFT\Interop\Interop.U8Distribute.dll
+            U8Distribute.iDistributeClass eaibroker = new U8Distribute.iDistributeClass();
+            try
+            {
+                String responseXml = eaibroker.Process(dom.OuterXml);
+                string s;
+                bool suc = moban.parseU8XmlRs(responseXml, out s);
+                if (suc)
+                    MessageBox.Show("导入成功！" + s);
+                else
+                    MessageBox.Show("导入失败！" + s);
+            }
+            finally
+            {
+                System.Runtime.InteropServices.Marshal.FinalReleaseComObject(eaibroker);       //COM释放
+            }
+        }
     }
 }
+ 
